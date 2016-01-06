e0cd62d25d69aa9ff0e95ecd4a96742d7e00eb03
cj1m <cj1mckay@gmail.com>
2016-01-06 21:56:14 +0000

    Loading images works but is slow

--- app/src/main/java/com/remember/alpha/CardViewAdapter.java
+++ app/src/main/java/com/remember/alpha/CardViewAdapter.java
@@ -1,6 +1,9 @@
 package com.remember.alpha;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 
 
@@ -33,18 +37,45 @@ public class CardViewAdapter extends RecyclerView.Adapter<CardViewAdapter.ViewHo
         TimeLines timeLine = countries.get(i);
         viewHolder.timelineName.setText(timeLine.name);
         viewHolder.timelineMembers.setText(timeLine.members);
-        if(timeLine.getImageResourceId(mContext) != -1)
+        if(timeLine.getImageUri() != null) {
             try {
-                viewHolder.timelineImage.setImageResource(timeLine.getImageResourceId(mContext));
-            } catch(Exception e) {
-
+                viewHolder.timelineImage.setImageBitmap(decodeUri(mContext,timeLine.getImageUri(), 100));
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-
+        }
     }
 
     @Override
     public int getItemCount() {
         return countries == null ? 0 : countries.size();
+    }
+
+    /*
+    * Used to avoid java.lang.OutOfMemoryError from loading
+    * images that are too large
+    */
+    public static Bitmap decodeUri(Context c, Uri uri, final int requiredSize)
+            throws FileNotFoundException {
+        BitmapFactory.Options o = new BitmapFactory.Options();
+        o.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(c.getContentResolver().openInputStream(uri), null, o);
+
+        int width_tmp = o.outWidth
+                , height_tmp = o.outHeight;
+        int scale = 1;
+
+        while(true) {
+            if(width_tmp / 2 < requiredSize || height_tmp / 2 < requiredSize)
+                break;
+            width_tmp /= 2;
+            height_tmp /= 2;
+            scale *= 2;
+        }
+
+        BitmapFactory.Options o2 = new BitmapFactory.Options();
+        o2.inSampleSize = scale;
+        return BitmapFactory.decodeStream(c.getContentResolver().openInputStream(uri), null, o2);
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
